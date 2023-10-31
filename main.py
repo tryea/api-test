ea661a94b0ef60a86b054743ff33ed1ec701b262
Ersapta Aristo <aristoersapta@gmail.com>
2023-10-31 11:21:32 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+data = {"name":"shopping cart",
+        "columns":["prod_name","price","num_items"],
+        "items":{}}
+
+@app.get("/")
+def root():
+    return {"message":"Welcome to Toko H8 Shopping Cart! There are some features that you can explore",
+            "menu":{1:"See shopping cart (/cart)",
+                    2:"Add item (/add) - You may need request",
+                    3:"Edit shopping cart (/edit/id)",
+                    4:"Delete item from shopping cart (/del/id)"}}
+
+@app.get("/cart")
+def show():
+    return data
+
+@app.post("/add")
+def add_item(added_item:dict):
+    id = len(data["items"].keys())+1
+    data["items"][id] = added_item
+    return f"Item successfully added into your cart with ID {id}"
+
+@app.put("/edit/{id}")
+def update_cart(id:int,updated_cart:dict):
+    if id not in data['items'].keys():
+        raise HTTPException(status_code=404, detail=f"Item with ID {id} not found")
+    else:
+        data["items"][id].update(updated_cart)
+        return {"message": f"Item with ID {id} has been updated successfully."}
+
+@app.delete("/del/{id}")
+def remove_row(id:int):
+    if id not in data['items'].keys():
+        raise HTTPException(status_code=404, detail=f"Item with ID {id} not found")
+    else:
+        data["items"].pop(id)
+        return {"message": f"Item with ID {id} has been deleted successfully."}
